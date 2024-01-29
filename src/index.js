feb9f584d32e8e6b8f7693024331b6fa7d981ce4
Alex Litv <trustywork@gmail.com>
2024-01-29 16:00:20 +0200

    v 1.0.0

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import fs from 'node:fs/promises';
+import path from 'node:path';
+import url from 'node:url';
+
+// get config dir
+const configDir = process.env.CONFIG_DIR || './config';
+
+const deepFreeze = obj => {
+    Object.keys(obj).forEach(prop => {
+        if (typeof obj[prop] === 'object' && !Object.isFrozen(obj[prop])) deepFreeze(obj[prop]);
+    });
+    return Object.freeze(obj);
+};
+
+const proxyHandler = {
+    get(target, filename, receiver) {
+        const filepath = path.join(configDir, `${filename}.js`);
+        const fileESM = url.pathToFileURL(filepath);
+
+        return import(fileESM).then((data) => deepFreeze(data.default));
+    },
+};
+
+const config = new Proxy(new Object(), proxyHandler);
+
+export default config;
